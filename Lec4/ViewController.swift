1ed30c7d2b2c0b6dbcc7e78fa2b0d73c71ae0c40
hackeru <hackeru@hackerus-Mac.local>
2017-08-31 17:54:23 +0300

    Initial Commit

---
+++ Lec4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lec4
+//
+//  Created by hackeru on 9 Elul 5777.
+//  Copyright © 5777 hackeru. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
