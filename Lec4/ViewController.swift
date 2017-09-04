daeeecd77415538a675d7d3e641ff248285b739d
hackerudroids <hackerudroids@gmail.com>
2017-09-04 18:32:37 +0300

    shuffle score

--- Lec4/ViewController.swift
+++ Lec4/ViewController.swift
@@ -15,45 +15,17 @@ class ViewController: UIViewController {
     @IBOutlet weak var topLabel: UILabel!
     @IBOutlet weak var bottomLabel: UILabel!
   
-    // var ca:[BlackJackCard] = []
-    // var car =  Array<BlackJackCard>()
+    @IBOutlet weak var scoreLabel: UILabel!
+    var deck = BlackjackDeck()
+    
     @IBAction func dealTapped(_ sender: UIButton) {
+        let card = deck.dealCard()
+        
+        midsLabel.text = card.suit.description
+        topLabel.text = card.rank.description
+        bottomLabel.text = card.rank.description
     }
-    
-    
-    
-    //prefer consts
-    //we init a blackjackcard: no new keyword:
-    //let card = BlackJackCard(rank: .Ace, suit: .Clubs)
-    //        //top -> Suit
-    //        topLabel.text = card.rank.description
-    //        //mid -> Suit
-    //        midsLabel.text = card.suit.description
-    //        //bottom -> Rank
-    //        bottomLabel.text = card.rank.description
-    //        //bottomLabel.transform = CGAffineTransform(rotationAngle: //)
-    //
-    //        //Optional<Integer> xx = new Optional<Integer>()
-    //        var x:Int? = nil
-    //
-    //        var suit:Suit? = Suit(rawValue: 0) //optional is like a warning from nils:
-    //
-    //
-    //        for i in 0...3{
-    //            //explicitly unwrapped optional
-    //            let s = Suit(rawValue: i)! //Hearts = 0, Spades, Diamonds, Clubs = 3
-    //            print(s.description)
-    //        }
-    
-    //        var s:Suit = .Hearts // Not optional
-    
-    //        if suit != nil{
-    //            //do something with suit
-    //            var str:String? = suit?.description
-    //        }
-    //
-    
-    
+ 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
